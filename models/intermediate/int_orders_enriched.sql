6b1b6e83eaa599a5894516ecc009ad02cb07ce24
Databirdie <evelyne.rouquet@orange.fr>
2025-04-20 17:14:09 +0200

    rework int_orders_enriched (#11)

--- models/intermediate/int_orders_enriched.sql
+++ models/intermediate/int_orders_enriched.sql
@@ -1,38 +1,41 @@
 select  
-    sto.store_name
+    o.store_id
+    , coalesce(sto.store_name, 'Unknown') as store_name
     , sto.store_address_zip_code
-    , sta.staff_first_name
-    , sta.staff_last_name
-    , c.customer_first_name
-    , c.customer_last_name
+    , o.staff_id
+    , coalesce(sta.staff_first_name, 'Unknown') as staff_first_name
+    , coalesce(sta.staff_last_name, 'Unknown') as staff_last_name
+    , o.customer_id
+    , coalesce(c.customer_first_name, 'Unknown') as customer_first_name
+    , coalesce(c.customer_last_name, 'Unknown') as customer_last_name
     , c.customer_address_zip_code
     , o.order_id
-    , p.product_name
-    , cat.product_category_name
-    , b.brand_name
+    , oi.product_id
+    , coalesce(p.product_name, 'Unknown') as product_name
+    , coalesce(cat.product_category_name, 'Unknown') as product_category_name
+    , coalesce(b.brand_name, 'Unknown') as brand_name
     , o.order_created_at
     , o.order_shipped_at
     , case when o.order_shipped_at < '2500-01-01' then true else false end as is_order_shipped
+    , case when o.order_shipped_at < '2500-01-01' then oi.ordered_quantity else 0 end as shipped_order_product_quantity
     , case when o.order_shipped_at = '2500-01-01' then oi.ordered_quantity else 0 end as pending_order_product_quantity
-    , oi.list_price
-    , oi.discount
     , case when o.order_shipped_at = '2500-01-01' then round(oi.ordered_quantity * oi.list_price,2) else 0 end as pending_order_turnover_before_discount
     , case when o.order_shipped_at = '2500-01-01' then round(oi.ordered_quantity * (oi.list_price - (oi.list_price * oi.discount)),2) else 0 end as pending_order_turnover
     , case when o.order_shipped_at < '2500-01-01' then round(oi.ordered_quantity * oi.list_price,2) else 0 end as order_turnover_before_discount
     , case when o.order_shipped_at < '2500-01-01' then round(oi.ordered_quantity * (oi.list_price - (oi.list_price * oi.discount)),2) else 0 end as order_turnover
 
 from  {{ ref('stg_orders') }} o 
-inner join {{ ref('stg_order_items') }} as oi 
+left join {{ ref('stg_order_items') }} as oi 
     on oi.order_id = o.order_id
-inner join {{ ref('stg_stores') }} as sto 
+left join {{ ref('stg_stores') }} as sto 
     on sto.store_id = o.store_id 
-inner join {{ ref('stg_staffs') }} as sta 
+left join {{ ref('stg_staffs') }} as sta 
     on sta.staff_id = o.staff_id 
-inner join {{ ref('stg_customers')}} as c 
+left join {{ ref('stg_customers')}} as c 
     on c.customer_id = o.customer_id
-inner join {{ ref('stg_products')}} as p 
+left join {{ ref('stg_products')}} as p 
     on p.product_id = oi.product_id
-inner join {{ ref('stg_brands')}} as b 
+left join {{ ref('stg_brands')}} as b 
     on b.brand_id = p.brand_id
-inner join {{ ref('stg_categories')}} as cat 
+left join {{ ref('stg_categories')}} as cat 
     on cat.product_category_id = p.category_id
